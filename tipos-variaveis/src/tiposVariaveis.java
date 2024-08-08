db72e2afaa211498362e325a0de29df86553e8d3
Danielferrazdev <daniel.f.mendonca@gmail.com>
2024-08-08 17:53:48 -0300

    Exemplo de tipo

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,12 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String  meuNome = "Daniel Ferraz";
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+        System.out.println(meuNome);
+    
     }
 }
