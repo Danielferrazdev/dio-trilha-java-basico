c35adf60a16ed2aadfe0e6bb26c232a38aa8c37d
Danielferrazdev <daniel.f.mendonca@gmail.com>
2024-09-09 17:21:37 -0300

    Teste DioCombo

--- dio-testes/execercicios-java/src/VerificacaoServicoFinal.java
+++ dio-testes/execercicios-java/src/VerificacaoServicoFinal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VerificacaoServico {
+public class VerificacaoServicoFinal {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
